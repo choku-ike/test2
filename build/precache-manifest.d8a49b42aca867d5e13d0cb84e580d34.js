ff49bd2b81ede1397d5b1c9fce9a0b2a04e11d4f
choku-ike <choku_ike@yahoo.co.jp>
2020-06-25 17:32:53 +0900

    react express sequelize(to azure) 最低限動作

---
+++ build/precache-manifest.d8a49b42aca867d5e13d0cb84e580d34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5b203d0700a23151536836ac8361c90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a6131747d6e6c8e2f9f",
+    "url": "/static/js/2.3f22e9c2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3f22e9c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe1ef3344c1e1f141b4d",
+    "url": "/static/js/main.4fdc3e90.chunk.js"
+  },
+  {
+    "revision": "f03e4bfabdf7b8dd33bd",
+    "url": "/static/js/runtime-main.175bc850.js"
+  }
+]);
--- server.js
+++ server.js
@@ -3,24 +3,36 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var methodOverride = require('method-override');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+//var indexRouter = require('./routes/index');
+//var usersRouter = require('./routes/users');
+var api = require('./routes/api');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(methodOverride('_method'));
+
+// correspond Express and React
+app.use(express.static(path.join(__dirname, 'build')));
+
+//app.use('/', indexRouter);
+//app.use('/users', usersRouter);
+app.use('/_api', api);
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -35,7 +47,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  //res.render('error');
 });
 
 module.exports = app;
